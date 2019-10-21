eabc46138afbef65a728974ad3a07f7c3a7cebc8
Rirdeg <52657039+Rirdeg@users.noreply.github.com>
2019-10-21 23:31:35 +0100

    lec-40
    Completed lec 40, added tilemap, restructured player engine tree

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -7,6 +7,8 @@ const GRAVITY = 250
 const UP = Vector2(0,-1)
 const JUMP_SPEED = 3500
 
+signal animate
+
 func _physics_process(delta):
 	apply_gravity()
 	jump()
@@ -31,17 +33,10 @@ func jump():
 func apply_gravity():
 	if is_on_floor():
 		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
 	else:
 		motion.y += GRAVITY
 		
 func animate():
-	if motion.y < 0:
-		$AnimatedSprite.play("jump")
-	elif motion.x > 0:
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("walk")
-	elif motion.x < 0: 
-		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.play("walk")
-	else:
-		$AnimatedSprite.play("idle")
+	emit_signal("animate", motion)
