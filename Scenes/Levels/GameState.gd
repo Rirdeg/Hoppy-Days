51dfeb021d61e82255c8b1fb592de7fb918661b0
Rirdeg <52657039+Rirdeg@users.noreply.github.com>
2019-10-28 22:37:44 +0000

    GUI, COINS AND BUG FIXES
    Added GUI for lives remaining, added collectible coins, fixes bug where player would often not react to spikes or jump pads

--- Scenes/Levels/GameState.gd
+++ Scenes/Levels/GameState.gd
@@ -1,15 +1,34 @@
 extends Node2D
 
 var lives = 3
+var coins = 0
+var life_plus_count = 10
 
 func _ready():
 	add_to_group("Gamestate")
+	update_GUI()
 
 func hurt():
 	lives -= 1
 	$Player.hurt()
+	update_GUI()
 	if lives < 0: 
 		end_game()
+
+func update_GUI():
+		get_tree().call_group("GUI","update_GUI",lives, coins)
 		
+
+func coin_up():
+	coins += 1
+	update_GUI()
+	var multiple_of_coins = (coins % life_plus_count) == 0
+	if multiple_of_coins:
+		life_up()
+
+func life_up():
+	lives += 1
+	update_GUI()
+
 func end_game():
 	get_tree().change_scene("res://Scenes/Levels/GameOver.tscn")
